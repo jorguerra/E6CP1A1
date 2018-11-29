8c73800251c226221abb3f2ccc542151cda46193
Jorge Guerra <jorguerra@gmail.com>
2018-11-28 21:31:45 -0300

    Ejercicios ifs

--- 1 if and else/ejercicio5.rb
+++ 1 if and else/ejercicio5.rb
@@ -4,12 +4,10 @@
 a = true
 b = true
 
-if a == true
-  if b == true
+if a == true && b == true
     puts 'Lograste A y B!'
-  else
+elsif a == true && b == false
     puts 'Lograste A! Pero no B!'
-  end
-else
+elsif a == false && b == false
   puts 'No lograste A ni B!'
 end
